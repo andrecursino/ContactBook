dad5b6ab9ab2af6a792ff5be27aa91f06d849068
andrecursino <andrephelipe@outlook.com>
2020-02-02 17:15:32 -0300

    renamed the units e excluded the unit uShowData

--- prjContactBook.dpr
+++ prjContactBook.dpr
@@ -2,14 +2,18 @@ program prjContactBook;
 
 uses
   Vcl.Forms,
-  uShowData in 'uShowData.pas' {Form1},
-  uPerson in 'uPerson.pas';
+  uPrincipal in 'uPrincipal.pas' {frmPrincipal},
+  uPerson in 'uPerson.pas',
+  uContactRegister in 'uContactRegister.pas' {frmContacts},
+  uTabGlobal in 'uTabGlobal.pas' {TabGlobal: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TfrmContacts, frmContacts);
+  Application.CreateForm(TTabGlobal, TabGlobal);
   Application.Run;
 end.
--- uPrincipal.pas
+++ uPrincipal.pas
@@ -4,11 +4,23 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, uPerson, Vcl.Menus;
+
 
 type
-  TForm2 = class(TForm)
-    lblData: TLabel;
+  TfrmPrincipal = class(TForm)
+    edtName: TEdit;
+    edtPhoneNumber: TEdit;
+    btnShow: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    MainMenu1: TMainMenu;
+    Register1: TMenuItem;
+    Contacts1: TMenuItem;
+    Exit1: TMenuItem;
+    procedure btnShowClick(Sender: TObject);
+    procedure Exit1Click(Sender: TObject);
+    procedure Contacts1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -16,10 +28,34 @@ type
   end;
 
 var
-  Form2: TForm2;
+  frmPrincipal: TfrmPrincipal;
 
 implementation
 
+uses
+  uContactRegister;
+
 {$R *.dfm}
+
+procedure TfrmPrincipal.btnShowClick(Sender: TObject);
+var
+  Contact : TContact;
+begin
+  Contact := TContact.Create;
+  Contact.Name := edtName.Text;
+  Contact.PhoneNumber := edtPhoneNumber.Text;
+  ShowMessage(Contact.ShowData);
+  Contact.Free;
+end;
+
+procedure TfrmPrincipal.Contacts1Click(Sender: TObject);
+begin
+  frmContacts.ShowModal;
+end;
+
+procedure TfrmPrincipal.Exit1Click(Sender: TObject);
+begin
+  Application.Terminate;
+end;
 
 end.
